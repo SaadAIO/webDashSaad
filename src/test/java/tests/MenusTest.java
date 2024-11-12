ca0654788d8c0adb8b3e4f902065e3e8d69ee04a
SAAD <saad.ali@aioapp.com>
2024-11-12 14:56:31 +0500

    Running through xml,using parameters for BaseURL

--- src/test/java/tests/MenusTest.java
+++ src/test/java/tests/MenusTest.java
@@ -12,30 +12,29 @@ import pages.menuManager.Menus;
 
 import java.time.Duration;
 
+import static basePage.BasePage.explicitWait;
+
 public class MenusTest extends BaseTest {
-    @Test
+    @Test(priority = 1)
     public void testValidLogin() {
         Menus loginDriver = new Menus(driver);
 
-        driver.get("https://main-dashboard.dev.aioapp.com/");
-
-
-       loginDriver.explicitWait(By.name("email"));
-        loginDriver.login("wasifQAr35@aioapp.com", "Test@1234");
-        loginDriver.explicitWait(By.xpath("//span[contains(text(),'Menu Manager')]"));
-
+        explicitWait(By.name("email"));
+        loginDriver.login("dash.auto@aioapp.com", "Test@1234");
+        explicitWait(By.xpath("//span[contains(text(),'Menu Manager')]"));
         loginDriver.clickMenuManagerButton();
-        loginDriver.explicitWait(By.xpath("//li[.='Menus']"));
+        explicitWait(By.xpath("//li[.='Menus']"));
         loginDriver.clickMenuButton();
-        loginDriver.explicitWait(By.xpath("//span[.='Create Menu ']"));
+        explicitWait(By.xpath("//span[.='Create Menu ']"));
         loginDriver.clickCreateMenuButton();
-        loginDriver.explicitWait(By.name("menuName"));
+        explicitWait(By.name("menuName"));
         loginDriver.enterMenuName();
         loginDriver.enterPOSName();
         loginDriver.enterColor();
         loginDriver.setSaveButton();
-        loginDriver.explicitWait(By.xpath("//h6[contains(text(),'Testmenu123')]"));
+        explicitWait(By.xpath("//h6[contains(text(),'Testmenu123')]"));
         Assert.assertTrue(driver.findElement(By.xpath("//h6[contains(text(),'Testmenu123')]")).isDisplayed(), "Successfully created");
-
     }
+
+
 }
